f64b28e857f30a96388c55eaa41da9c7207fbd8f
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-07-16 19:00:12 +0545

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ✨ Motion Basics
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-center text-4xl font-semibold">Motion Basics</h1>
+    </main>
+  );
+}
